f11a5bb4e9a7eeace8e9ccdb5adb973047673f82
Stanislav <verbatim8872@gmail.com>
2021-10-05 14:57:20 +0300

    add binarySearchRecursive

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,5 +1,6 @@
 const array = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ];
 
+let count = 0;
 
 // O(log 2n) - работает только с отсортированными массивами
 const binarySearchCycle = (array, searchValue) => {
@@ -7,7 +8,6 @@ const binarySearchCycle = (array, searchValue) => {
     let end = array.length;
     let middle;
     let complete = false;
-    let count = 0;
 
     while (!complete && start <= end) {
 
@@ -31,4 +31,22 @@ const binarySearchCycle = (array, searchValue) => {
 
 }
 
-console.log(binarySearchCycle(array, 14));
+const binarySearchRecursive = (array, searchValue, start, end) => {
+    const middle = Math.floor( (end + start) / 2 );
+    count ++;
+
+    if (array[middle] === searchValue) {
+        return `Элемент ${searchValue} по индексу ${middle} был найден за ${count} итераций`;
+    }
+
+    if (array[middle] < searchValue) {
+        return binarySearchRecursive(array, searchValue, middle + 1, end);
+    }
+
+    if (array[middle] > searchValue) {
+        return binarySearchRecursive(array, searchValue, start, middle - 1);
+    }
+}
+
+// console.log(binarySearchCycle(array, 14));
+console.log(binarySearchRecursive(array, 14, 0, array.length));
