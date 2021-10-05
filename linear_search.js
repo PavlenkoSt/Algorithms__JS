0623feb47d9bd4bd341569259f693c8520acd7cc
Stanislav <verbatim8872@gmail.com>
2021-10-05 13:31:36 +0300

    linear

---
+++ linear_search.js
@@ -0,0 +1,14 @@
+const array = [ 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+const linearSearch = (arr, searchValue) => {
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        count ++;
+        if(arr[i] === searchValue){
+            return `Элемент ${searchValue} по индексу ${i} был найден за ${count} итераций`;
+        }
+    }
+    return 'Элемент не найден';
+}
+
+console.log(linearSearch(array, 8));
